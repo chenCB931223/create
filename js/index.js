c54e97fc4b9722e3ea9ff0fa9f2647c30d3ea4b8
chenCB931223 <42188252+chenCB931223@users.noreply.github.com>
2018-11-04 14:19:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,117 +1,127 @@
 $(function() {
-    var trends = new Trends();
+    var trends = new Trends($('.box'));
 })
 
 class Trends {
-    constructor() {
-        this.box = $('.box');
-        this.addEvent();
-
+    constructor(parent) {
+        this.box = $(parent);
+        this.grounp = [];
+        this.addEvent()
     }
     create() {
-        let strDom = '<div class="create_box">' +
-            '<header class="header">' +
-            '<div class="header_box">' +
-            '<span class="small">_</span>' +
-            '<span class="big">口</span>' +
-            '<span class="max">回</span>' +
-            '<span class="clear">x</span>' +
-            '</div>' +
-            '</header>' +
-            '<div class="content">' +
-            '<p class="text"></p>' +
-            '</div>' +
-            '<footer class="footer"></footer>' +
-            '<div class="left"></div>' +
-            '<div class="right"></div>' +
-            '<div class="top"></div>' +
-            '<div class="bottom"></div>' +
-            '<div class="u-l"></div>' +
-            '<div class="u-r"></div>' +
-            '<div class="b-l"></div>' +
-            '<div class="b-r"></div>' +
-            '</div>';
+        const strDom = `<div class="create_box">
+            <header class="header">
+            <div class="header_box">
+            <a class="small">_</a>
+            <a class="big"></a>
+            <a class="max"></a>
+            <a class="clear">x</a>
+            </div>
+            </header>
+            <div class="content">
+            <p class="text"></p>
+            </div>
+            <div class="left"></div>
+            <div class="right"></div>
+            <div class="top"></div>
+            <div class="bottom"></div>
+            <div class="u-l"></div>
+            <div class="u-r"></div>
+            <div class="b-l"></div>
+            <div class="b-r"></div>
+            </div>`;
         this.box.append(strDom);
     }
     addEvent() {
-        let _this = this;
+        const _this = this;
         $('body').on('click', '#btn', function() {
             _this.create();
+
+            _this.getGrounp();
             _this.getSize();
-            _this.move();
 
-            //四边
-            _this.drawing($('.top'), false, true, false, true);
-            _this.drawing($('.left'), true, false, true, false);
-            _this.drawing($('.bottom'), false, false, false, true);
-            _this.drawing($('.right'), false, false, true, false);
-            //四角
-            _this.drawing($('.b-r'), false, false, true, true);
-            _this.drawing($('.u-r'), false, true, true, true);
-            _this.drawing($('.u-l'), true, true, true, true);
-            _this.drawing($('.b-l'), true, false, true, true);
         });
+    }
+    getGrounp() {
+        const _this = this;
+        this.create_box = $('.create_box');
+        this.grounp.length = 0;
+        this.create_box.each(function() {
+            _this.grounp.push(this);
 
+            _this.move(this);
+            //四边
+            _this.drawing(this, $(this).find('.top'), false, true, false, true);
+            _this.drawing(this, $(this).find('.left'), true, false, true, false);
+            _this.drawing(this, $(this).find('.bottom'), false, false, false, true);
+            _this.drawing(this, $(this).find('.right'), false, false, true, false);
+            //四角
+            _this.drawing(this, $(this).find('.b-r'), false, false, true, true);
+            _this.drawing(this, $(this).find('.u-r'), false, true, true, true);
+            _this.drawing(this, $(this).find('.u-l'), true, true, true, true);
+            _this.drawing(this, $(this).find('.b-l'), true, false, true, true);
 
+        });
     }
     getSize() {
-        let width = $('#width').val(),
-            height = $('#height').val();
-        let _this = this;
-        let gentle = $('.header').height() + $('.footer').height();
-        $('.create_box').css({
+        const _this = this;
+        let len = this.grounp.length;
+        const width = $('#width').val(),
+            height = $('#height').val(),
+            create_box = $('.create_box');
+        const casket = $(this.grounp[len - 1]);
+
+        casket.css({
             'width': width + 'px',
             'height': height + 'px'
         });
-        $('.text').html($('#text').val());
-        $('.content').css('height', $('.create_box').height() - gentle + 'px');
 
-        $('.big').on('click', function() {
-            $('.create_box').css({
+        casket.find('.text').html($('#text').val());
+        this.create_box.find('.big').on('click', function() {
+            $(this).closest(create_box).css({
                 'width': _this.box.width() + 'px',
                 'height': _this.box.height() + 'px',
                 'top': '0',
                 'left': '0'
             });
-            $('.content').css('height', $('.create_box').height() - 60 + 'px');
-            $('.max').show();
-            $('.big').hide();
+            $(this).siblings('.max').show();
+            $(this).hide();
         });
-        $('.max').on('click', function() {
+        this.create_box.find('.max').on('click', function() {
             $('.max').hide();
             $('.big').show();
-            $('.create_box').css({
+            $(this).closest(create_box).css({
                 'width': width + 'px',
                 'height': height + 'px',
                 'top': '10px',
                 'left': '10px'
             });
         })
-        $('.small').on('click', function() {
+        this.create_box.find('.small').on('click', function() {
 
             $('.content').slideToggle(200);
 
-
+            create_box.css('height', $('.header').height());
 
         });
 
-        $('.clear').on('click', function() {
-            $('.create_box').remove();
+        this.create_box.find('.clear').on('click', function() {
+            $(this).closest(create_box).remove();
         });
 
     }
-    move() {
+    move(parent) {
         const active = function(e) {
-            let $box_w = $('.box').width(),
-                $box_h = $('.box').height(),
-                create_box = $('.create_box');
-            let create_box_w = create_box.width(),
-                create_box_h = create_box.height();
-                e.stopPropagation();
+            e.stopPropagation();
+            const $box_w = $('.box').width(),
+                $box_h = $('.box').height();
+            let create_box_w = $(parent).width(),
+                create_box_h = $(parent).height();
+
             let ox = e.clientX,
                 oy = e.clientY;
-            let create_box_left = create_box.position().left,
-                create_box_top = create_box.position().top;
+            let create_box_left = $(parent).position().left,
+                create_box_top = $(parent).position().top;
 
             const handle = function(e) {
                 let left = e.clientX - ox,
@@ -120,7 +130,7 @@ class Trends {
                     to_t = create_box_top + top;
                 to_t = Math.min($box_h - create_box_h, Math.max(0, to_t));
                 to_l = Math.min($box_w - create_box_w, Math.max(0, to_l));
-                $('.create_box').css({
+                $(parent).css({
                     'left': to_l,
                     'top': to_t
                 })
@@ -131,39 +141,39 @@ class Trends {
                 $(document).off('mousemove', handle);
             })
         }
-        $('.header').on('mousedown', active);
+        $(parent).find('.header').on('mousedown', active);
     }
-    drawing(handle, isleft, istop, lookx, looky) {
-        let minW = $('#width').val(),
-            minh = $('#height').val();
-        let _this = this;
+    drawing(parent, name, isleft, istop, isright, isbottom) {
+
+        const minW = 200,
+            minh = 200;
+        const _this = this;
         let disX = 0,
             disY = 0;
-        $(handle).on('mousedown', function(e) {
-            let create_box = $('.create_box');
+        const active = function(e) {
             let box_w = _this.box.width(),
                 box_h = _this.box.height(),
-                create_w = create_box.width(),
-                create_h = create_box.height(),
-                create_left = create_box.position().left,
-                create_top = create_box.position().top;
+                create_w = $(parent).width(),
+                create_h = $(parent).height(),
+                create_left = $(parent).position().left,
+                create_top = $(parent).position().top;
 
             disX = e.clientX - $(this).position().left;
             disY = e.clientY - $(this).position().top;
 
-            $(document).on('mousemove', function(e) {
+            const handle = function(e) {
                 let it = e.clientY - disY,
                     il = e.clientX - disX;
-                let maxw = box_w - create_box.position().left - 2,
-                    maxh = box_h - create_box.position().top - 2;
-                let iw = isleft ? create_w - il : $(handle).width() + il;
-                let ih = istop ? create_h - it : $(handle).height() + it;
+                let maxw = box_w - $(parent).position().left - 2,
+                    maxh = box_h - $(parent).position().top - 2;
+                let iw = isleft ? create_w - il : $(name).width() + il;
+                let ih = istop ? create_h - it : $(name).height() + it;
 
                 if (isleft) {
-                    create_box.css('left', create_left + il);
+                    $(parent).css('left', create_left + il);
                 }
                 if (istop) {
-                    create_box.css('top', it + create_top + 'px');
+                    $(parent).css('top', it + create_top + 'px');
                 }
 
                 if (iw < minW) {
@@ -176,21 +186,25 @@ class Trends {
                 } else if (ih > maxh) {
                     ih = maxh;
                 }
-                if (lookx) {
-                    create_box.css('width', iw + 'px');
+                if (isright) {
+                    $(parent).css('width', iw + 'px');
                 }
-                if (looky) {
-                    create_box.css('height', ih + 'px');
+                if (isbottom) {
+                    $(parent).css('height', ih + 'px');
                 }
-
                 if ((isleft && iw == minW) || (istop && ih == minh)) {
-                    $(document).off();
+                    $(document).off('mousemove', handle);
+                    $(document).off('mousedown', active);
+
                 };
-            });
+            };
+
+            $(document).on('mousemove', handle);
             $(document).on('mouseup', function() {
-                $(document).off();
+                $(document).off('mousemove', handle);
             });
-        })
+        };
+        $(name).on('mousedown', active);
 
     }
 }
